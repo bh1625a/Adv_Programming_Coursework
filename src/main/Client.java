3cbe796ddebd105c62ac5f9acd014a821e195a4c
Benjamin <benj@minclifford.co.uk>
2021-03-04 17:32:37 +0000

    Add Client Methods

--- src/main/Client.java
+++ src/main/Client.java
@@ -7,9 +7,63 @@ public class Client extends Thread {
     private LoginWindow loginWindow;
     private ChatWindow chatWindow;
     private Socket socket;
+    private ClientHelper clientHelper;
+    private boolean isConnected = false;
 
-    public static void main(String[] args) throws IOException {
-        ClientHelper clientHelper = new ClientHelper();
+    private int serverPort;
+    private String serverAddress;
 
+
+    public Client(ClientHelper clientHelper){
+        super.start();
+        this.clientHelper = clientHelper;
+    }
+
+    public void run(){
+
+    }
+
+    public void openLoginWindow(){
+
+    }
+
+    public void openChatWindow(){
+
+    }
+
+    public void connectToServer(){
+
+    }
+
+    public void setIsConnected(){
+
+    }
+
+    public boolean isConnected(){
+        return isConnected;
+    }
+
+    public void connectionStatusChange(){
+
+    }
+
+    public void sendGUIExit(){
+
+    }
+
+    public void setServerAddress(String serverAddress) {
+        this.serverAddress = serverAddress;
+    }
+
+    public String getServerAddress() {
+        return serverAddress;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public int getServerPort() {
+        return serverPort;
     }
 }
