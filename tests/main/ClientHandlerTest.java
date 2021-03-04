68488765f793356d64e89cac8000376720d89f9b
maninandra <mn1453e@gre.ac.uk>
2021-03-04 17:07:43 +0000

    ClientHandlerTest added

---
+++ tests/main/ClientHandlerTest.java
@@ -0,0 +1,33 @@
+package main;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientHandlerTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void sendMessage() {
+    }
+
+    @Test
+    void setID() {
+    }
+
+    @Test
+    void getID() {
+    }
+
+    @Test
+    void sendCurrentUserList() {
+    }
+
+    @Test
+    void run() {
+    }
+}
