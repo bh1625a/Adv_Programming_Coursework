46aba71cd7f9a0f323bd7a16c58b7d8132864fe8
maninandra <mn1453e@gre.ac.uk>
2021-03-11 23:17:15 +0000

    sendMemberDetails and displayMembers methods

--- src/main/ClientHelper.java
+++ src/main/ClientHelper.java
@@ -1,7 +1,23 @@
 package main;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ClientHelper extends Thread {
     private Client client;
+    private ClientHelper clientHelper;
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String message = "";
+    private String userList;
+    private String clientPort;
+    private ArrayList<String> userArrayList;
+    private InputStreamReader isr;
+
+
+
 
     public ClientHelper(){
         super.start();
@@ -11,4 +27,44 @@ public class ClientHelper extends Thread {
         client = new Client(this);
     }
 
+
+    public void sendMemberDetails() throws IOException {
+        try {
+            socket = new Socket();
+            clientPort = String.valueOf(client.getClientPort());
+
+            while (client.isConnected()) {
+                out = new PrintWriter(socket.getOutputStream(), true);
+                out.println("SUBMITUSERLIST");
+                userList = client.getClientAddress() + "," + clientPort;
+                out.println(userList);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void displayMembers() throws IOException {
+        socket = new Socket();
+        isr = new InputStreamReader(System.in);
+        in = new BufferedReader(isr);
+
+        while (true){
+            while (message == in.readLine()){
+                if (message != null){
+                    if (message.equals("ALLUSERS")){
+                        String list = "";
+                        while (list == in.readLine()){
+                            userArrayList.add(list);
+
+                        }
+                        if(message.contains("END")){
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
 }
