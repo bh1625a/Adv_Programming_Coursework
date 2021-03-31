b0004ab5d57ccf344bf1d9bb5455af97d104270d
maninandra <mn1453e@gre.ac.uk>
2021-03-31 16:45:16 +0100

    Unit Testing

--- tests/main/CoordinatorTest.java
+++ tests/main/CoordinatorTest.java
@@ -1,16 +1,43 @@
 package main;
 
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoordinatorTest {
-    private Coordinator coordinator = new Coordinator();
+    private Coordinator coordinator;
+    private ArrayList<String> memberList;
     private ArrayList<String> ids;
+    private HashMap <String, Integer> counterMap;
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private Socket connect;
+    private Scanner in;
+    private PrintWriter out;
 
-
+    @BeforeClass
+    public void initialise() throws IOException {
+        serverSocket = new ServerSocket(58001);
+        socket = new Socket("localhost", 58001);
+        connect = serverSocket.accept();
+        in = new Scanner(connect.getInputStream());
+        out = new PrintWriter(connect.getOutputStream(), true);
+    }
+    @BeforeEach
+    public void setup(){
+        coordinator = new Coordinator();
+    }
 
     @Test
     void getId() {
@@ -49,15 +76,27 @@ class CoordinatorTest {
     }
 
     @Test
-    void checkConnectionStatus() {
+    void buildHashMap() throws IOException {
+        memberList = new ArrayList<>();
+        ids = new ArrayList<>();
+        memberList.add("Benjamin:59002:192.168.0.60");
+        memberList.add("Robbie:59006:192.168.0.60");
+
+        for(String clients: memberList){
+            String[] parts = clients.split(":");
+            String userID = parts[0];
+            ids.add(userID);
+        }
+        assertEquals(2, ids.size());
+
+        counterMap = new HashMap<>();
+
+       for (String members: ids){
+            counterMap.put(members, 0);
+        }
+        assertEquals(2 ,counterMap.size());
+        assertEquals(true , counterMap.containsKey("Benjamin"));
     }
 
-    @Test
-    void buildHashMap() {
-        
-    }
 
-    @Test
-    void checkPong() {
-    }
 }
